77b58a1097bdcb35bce7c383bab0bb07c8e6c142
frankogenrwoth <ogenrwothjimfrank@gmail.com>
2026-02-04 16:51:45 +0300

    refactor: Restructure Telescope configuration with new keymaps and layout options, and remove local development plugin path.

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,20 +1,50 @@
 return {
   {
     'nvim-telescope/telescope.nvim',
+    version = '*',
     dependencies = {
       'nvim-lua/plenary.nvim',
       { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
     },
-    config = function()
-      -- Telescope setup
-      vim.keymap.set('n', '<leader>en', function()
-        require('telescope.builtin').find_files {
-          cwd = vim.fn.stdpath 'config',
-        }
-      end, { desc = 'edit neovim' })
-
-      -- General Telescope keymaps
-      vim.keymap.set('n', '<leader>fd', require('telescope.builtin').find_files, { desc = 'Telescope Find Files' })
+    cmd = 'Telescope',
+    keys = {
+      { '<leader>ff', '<cmd>Telescope find_files<cr>', desc = 'Find Files' },
+      { '<leader>fg', '<cmd>Telescope live_grep<cr>', desc = 'Live Grep' },
+      { '<leader>f3', '<cmd>Telescope git_files<cr>', desc = 'Git Files' },
+      { '<leader>fb', '<cmd>Telescope buffers<cr>', desc = 'Find Buffers' },
+      { '<leader>fh', '<cmd>Telescope help_tags<cr>', desc = 'Help Tags' },
+    },
+    opts = {
+      mappings = {
+        i = {
+          ['<C-j>'] = 'move_selection_next',
+          ['<C-k>'] = 'move_selection_previous',
+          ['<C-u>'] = false, -- disable page up
+          ['<C-d>'] = false, -- disable page down
+        },
+        n = {
+          ['q'] = 'close',
+        },
+      },
+      layout_strategy = 'flex',
+      layout_config = {
+        horizontal = {
+          preview_width = 0.55,
+          results_width = 0.8,
+        },
+        vertical = {
+          mirror = false,
+        },
+        flex = {
+          flip_columns = 160,
+        },
+      },
+      path_display = { 'smart' },
+    },
+    config = function(_, opts)
+      local telescope = require 'telescope'
+      telescope.setup(opts)
+      telescope.load_extension 'fzf'
     end,
   },
 }
